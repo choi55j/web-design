7b5690f47af2c26e7b781a36ac2cbdd2d7c88b4b
최수혁(choi soo hyeok) <121610467+choi55j@users.noreply.github.com>
2024-05-28 11:50:12 +0900

    Add files via upload

---
+++ 30916_0528 가로수행/script.js
@@ -0,0 +1,17 @@
+$('.navi>li').mouseover(function(){
+    $(this).children('.sub').stop().slideDown(200);
+});
+
+$('.navi>li').mouseout(function(){
+    $(this).children('.sub').stop().slideUp(200);
+});
+
+setInterval(function(){
+    $('.imgslide').delay(1000);
+    $('.imgslide').animate({marginTop : -300})
+    $('.imgslide').delay(2000);
+    $('.imgslide').animate({marginTop : -600})
+    $('.imgslide').delay(2000);
+    $('.imgslide').animate({marginTop : 0})
+    $('.imgslide').delay(3000);
+})
