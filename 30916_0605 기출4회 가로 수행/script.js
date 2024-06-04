8f51ec573d58963a7867e28d31656f2b49a11085
최수혁(choi soo hyeok) <121610467+choi55j@users.noreply.github.com>
2024-06-05 01:39:59 +0900

    Add files via upload

---
+++ 30916_0605 기출4회 가로 수행/script.js
@@ -0,0 +1,32 @@
+$('.navi>li').mouseover(function(){
+    $(this).children('.sub').stop().slideDown(200);
+});
+
+$('.navi>li').mouseout(function(){
+    $(this).children('.sub').stop().slideUp(200);
+});
+
+setInterval(function(){
+    $('.imgslide').delay(2000);
+    $('.imgslide').animate({marginTop:-300})
+    $('.imgslide').delay(2000);
+    $('.imgslide').animate({marginTop:-600})
+    $('.imgslide').delay(2000);
+    $('.imgslide').animate({marginTop:0})
+    $('.imgslide').delay(2000);
+})
+
+$(function(){
+    $('.tabmenu>li>a').click(function(){
+        $(this).parent().addClass('active').siblings().removeClass('active');
+        return false;
+    });
+});
+
+$('.notice li:first').click(function(){
+    $('#modal').addClass('active');
+});
+
+$('.btn').click(function(){
+    $('#modal').removeClass('active');
+});
