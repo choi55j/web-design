418b915b492f526acb9fa73dd9458c6505d2b905
최수혁(choi soo hyeok) <121610467+choi55j@users.noreply.github.com>
2024-06-13 16:15:33 +0900

    Add files via upload

---
+++ 30916_0613 세로 시작/script.js
@@ -0,0 +1,7 @@
+$('.navi li').mouseover(function(){
+    $(this).children('.sub').stop().slideDown(200);
+});
+
+$('.navi li').mouseout(function(){
+    $(this).children('.sub').stop().slideUp(200);
+});
