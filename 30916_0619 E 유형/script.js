4132f76479146cd7cf3756f29f1829dfe377457e
최수혁(choi soo hyeok) <121610467+choi55j@users.noreply.github.com>
2024-06-19 12:50:51 +0900

    Add files via upload

--- 30916_0619 E 유형/script.js
+++ 30916_0619 E 유형/script.js
@@ -5,3 +5,12 @@ $('.navi li').mouseover(function(){
 $('.navi li').mouseout(function(){
     $(this).children('.sub').stop().slideUp(200);
 });
+
+setInterval(function(){
+    $('.imgslide').delay(2000);
+    $('.imgslide').animate({marginLeft:-1400});
+    $('.imgslide').delay(2000);
+    $('.imgslide').animate({marginLeft:-2800});
+    $('.imgslide').delay(2000);
+    $('.imgslide').animate({marginLeft: 0});
+});
