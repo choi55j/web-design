6fd07a28a4d0dbe5d88223c162a658aa4dda7934
최수혁(choi soo hyeok) <121610467+choi55j@users.noreply.github.com>
2024-06-20 16:40:52 +0900

    Add files via upload

---
+++ 30916_0620 E유형 완성/script.js
@@ -0,0 +1,24 @@
+$('.navi li').mouseover(function () {
+    $(this).children('.sub').stop().slideDown(200);
+});
+
+$('.navi li').mouseout(function () {
+    $(this).children('.sub').stop().slideUp(200);
+});
+
+setInterval(function () {
+    $('.imgslide').delay(2000);
+    $('.imgslide').animate({ marginLeft: -200 + 'vh' });
+    $('.imgslide').delay(2000);
+    $('.imgslide').animate({ marginLeft: -400 + 'vh' });
+    $('.imgslide').delay(2000);
+    $('.imgslide').animate({ marginLeft: 0 });
+});
+
+$('.notice ul>li:first').click(function () {
+    $('#modal').addClass('active');
+});
+
+$('.btn').click(function () {
+    $('#modal').removeClass('active');
+});
