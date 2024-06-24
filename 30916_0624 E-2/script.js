059f5c4446cf48fa97d0edf6492eb74f25328fc0
최수혁(choi soo hyeok) <121610467+choi55j@users.noreply.github.com>
2024-06-24 17:49:04 +0900

    Delete 30916_0624 E-2 directory

--- 30916_0624 E-2/script.js
+++
@@ -1,7 +0,0 @@
-$('.navi li').mouseover(function(){
-    $(this).children('.sub').stop().slideDown(200);
-});
-
-$('.navi li').mouseout(function(){
-    $(this).children('.sub').stop().slideUp(200);
-});
